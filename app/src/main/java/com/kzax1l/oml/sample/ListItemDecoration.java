3428ba35a3355108004c810076dd748cec09ea33
Zsago <3427241441@qq.com>
2017-07-16 20:14:42 +0800

    add RecyclerActivity

---
+++ app/src/main/java/com/kzax1l/oml/sample/ListItemDecoration.java
@@ -0,0 +1,64 @@
+package com.kzax1l.oml.sample;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Zsago on 2017/3/8.
+ *
+ * @since 2.0.1
+ */
+public class ListItemDecoration extends RecyclerView.ItemDecoration {
+    private Drawable divider;
+    private int dividerHeight;
+
+    public ListItemDecoration(Context context) {
+        TypedArray ta = context.obtainStyledAttributes(new int[]{android.R.attr.listDivider});
+        divider = ta.getDrawable(0);
+        ta.recycle();
+        dividerHeight = 2;
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+        boolean signed = false;
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+        for (int i = 0; i < parent.getChildCount(); i++) {
+            View child = parent.getChildAt(i);
+            if (!signed && child instanceof ViewGroup) {
+                ViewGroup item = (ViewGroup) child;
+                for (int j = 0; j < item.getChildCount(); j++) {
+                    if (item.getChildAt(j) instanceof ImageView) {
+                        ImageView imageView = (ImageView) item.getChildAt(j);
+                        left += imageView.getRight();
+                        signed = true;
+                    }
+                }
+            }
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+            int top = child.getBottom() + params.topMargin;
+            int bottom = top + dividerHeight;
+            divider.setBounds(left, top, right - child.getPaddingRight(), bottom);
+            divider.draw(c);
+        }
+    }
+
+    @Override
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDrawOver(c, parent, state);
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+    }
+}
---
+++ app/src/main/java/com/kzax1l/oml/sample/RecyclerActivity.java
@@ -0,0 +1,58 @@
+package com.kzax1l.oml.sample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kzax1l.oml.OMLApplication;
+
+/**
+ * Created by Zsago on 2017/7/16.
+ *
+ * @author Zsago
+ */
+
+public class RecyclerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        recyclerView.addItemDecoration(new MyItemDecoration());
+        recyclerView.setAdapter(new Adapter());
+    }
+
+    class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.channel_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.textView.setText(((OMLApplication) getApplication()).getChannelManage().getUserChannel().get(position).name);
+        }
+
+        @Override
+        public int getItemCount() {
+            return ((OMLApplication) getApplication()).getChannelManage().getUserChannel().size();
+        }
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView.findViewById(R.id.text_item);
+        }
+    }
+}
