01928ca0fbcca0fc3d01e50a654b805619b29be7
Zsago <3427241441@qq.com>
2018-01-18 17:51:44 +0800

    测试自定义模块布局配置项

--- app/src/main/java/com/kzax1l/oml/sample/WorkApplication.java
+++ app/src/main/java/com/kzax1l/oml/sample/WorkApplication.java
@@ -1,8 +1,20 @@
 package com.kzax1l.oml.sample;
 
-import com.kzax1l.oml.OMLApplication;
+import android.app.Application;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kzax1l.oml.OMLApplicationAgent;
+import com.kzax1l.oml.OMLModuleOptions;
+import com.kzax1l.oml.OMLModuleProvider;
+import com.kzax1l.oml.dao.ModuleItem;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2018-01-11.
@@ -10,13 +22,21 @@ import com.squareup.leakcanary.RefWatcher;
  * @author kzaxil
  * @since 1.0.1
  */
-public class WorkApplication extends OMLApplication {
+public class WorkApplication extends Application implements OMLModuleProvider, OMLModuleOptions {
     private RefWatcher refWatcher;
+    private OMLApplicationAgent mAgent = new OMLApplicationAgent(this, this, this);
 
     @Override
     public void onCreate() {
         super.onCreate();
         refWatcher = setupLeakCanary();
+        mAgent.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        mAgent.onTerminate();
     }
 
     private RefWatcher setupLeakCanary() {
@@ -34,5 +54,37 @@ public class WorkApplication extends OMLApplication {
      */
     public RefWatcher getRefWatcher() {
         return refWatcher;
+    }
+
+    @Override
+    public List<ModuleItem> available() {
+        List<ModuleItem> defaultModules = new ArrayList<>();
+        defaultModules.add(new ModuleItem("场所档案", "module_place"));
+        defaultModules.add(new ModuleItem("身份验证", "module_identification"));
+        defaultModules.add(new ModuleItem("场所检查", "module_enterprise"));
+        defaultModules.add(new ModuleItem("现场勘查", "module_spot"));
+        defaultModules.add(new ModuleItem("特行年检", "module_annual_survey"));
+        defaultModules.add(new ModuleItem("快递抽查", "module_deliver"));
+        defaultModules.add(new ModuleItem("报警/预警处理", "module_ring"));
+        defaultModules.add(new ModuleItem("法律法规", "module_law"));
+        return defaultModules;
+    }
+
+    @Override
+    public List<ModuleItem> unavailable() {
+        List<ModuleItem> defaultModules = new ArrayList<>();
+        defaultModules.add(new ModuleItem("危化盘库异常处理", "module_dangerous"));
+        defaultModules.add(new ModuleItem("旅馆验收", "module_hotel"));
+        return defaultModules;
+    }
+
+    @Override
+    public View moduleLayout(Context context, ViewGroup parent) {
+        return LayoutInflater.from(this).inflate(R.layout.item_module, parent, false);
+    }
+
+    @Override
+    public int version() {
+        return 0;
     }
 }
